d6e69428ffb150f90a794186034deba6f726caa3
Ina Krapp <ina.krapp@freenet.de>
2026-08-31 21:50:30 +0200

    Added presentation.

---
+++ webshot_code.R
@@ -0,0 +1,4 @@
+library(here)
+webshot2::webshot(here("presentations/LLM_assistants/Ina Krapp – Coding Assistants in RStudio and Visual Studio Code.html"),
+                  file = here("presentations/LLM_assistants/thumbnail.png"),
+                  vwidth = 1280, vheight = 720)
